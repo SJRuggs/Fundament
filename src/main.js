9513ee1078fe066aab8859a8ac0b2fc7257dc3a0
SJRuggs <sjruggs03@gmail.com>
2023-09-09 02:52:36 -0400

    cursor and hover working, json read

--- src/main.js
+++ src/main.js
@@ -1,37 +1,9 @@
-// import loadJSON from "./utils.js";
+import setupTooltip from "./utils.js";
 
 const init = data => 
 {
-    console.log(data);
-
-    const button = document.querySelector(".hover");
-
-    button.addEventListener('mouseover', (e) =>
-    {
-        console.log("entering mouseover area");
-    })
-
-    button.addEventListener('mouseout', () =>
-    {
-        console.log("exiting mouseover area");
-    })
-
-    let tooltip = document.querySelectorAll('.tooltip');
-
-    document.addEventListener('mousemove', fn);
-
-    function fn(e) {
-        for (let i = tooltip.length; i--;)
-        {
-            e.pageX > window.innerWidth / 2
-                ? tooltip[i].style.left = (e.pageX - parseInt(getComputedStyle(tooltip[i]).width) - 4 * parseInt(getComputedStyle(tooltip[i]).marginRight)) + 'px'
-                : tooltip[i].style.left = e.pageX + 'px';
-            e.pageY > window.innerHeight / 2
-                ? tooltip[i].style.top = (e.pageY - parseInt(getComputedStyle(tooltip[i]).height) - 4 * parseInt(getComputedStyle(tooltip[i]).marginBottom)) + 'px'
-                : tooltip[i].style.top = e.pageY + 'px';
-        }
-    }
-
+    setupTooltip();
+    setupCharacterSheet(data);
 
 
 
@@ -49,3 +21,8 @@ const init = data =>
 fetch("./data/character.json")
     .then((response) => response.json())
     .then((json) => init(json));
+
+const setupCharacterSheet = data =>
+{
+    document.querySelector("#title").innerHTML = data.name;
+}
--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,19 @@
+const setupTooltip = () =>
+{
+    document.addEventListener('mousemove', fn);
+    let tooltip = document.querySelectorAll('.tooltip');
+    function fn(e) {
+        for (let i = tooltip.length; i--;)
+        {
+            e.pageX > window.innerWidth / 2
+                ? tooltip[i].style.left = (e.pageX - parseInt(getComputedStyle(tooltip[i]).width) - 4 * parseInt(getComputedStyle(tooltip[i]).marginRight)) + 'px'
+                : tooltip[i].style.left = e.pageX + 'px';
+            e.pageY > window.innerHeight / 2
+                ? tooltip[i].style.top = (e.pageY - parseInt(getComputedStyle(tooltip[i]).height) - 4 * parseInt(getComputedStyle(tooltip[i]).marginBottom)) + 'px'
+                : tooltip[i].style.top = e.pageY + 'px';
+        }
+    }
+    console.log("tooltip imported");
+}
 
-// export default functionName;
+export default setupTooltip;
